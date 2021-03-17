392ffecae0f36d6d2fa6e4f3a6dd0fb316be86e7
mateusz pienkowski <mateuszp2286@gmail.com>
2021-03-17 18:53:12 +0100

    Added persistent history with json
    Added json files serialization and deserialization

--- Keyboard/ViewModels/MainWindowViewModel.cs
+++ Keyboard/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,4 @@
-﻿using Prism.Mvvm;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +9,9 @@ using Prism.Commands;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
 using Keyboard.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
 
 namespace Keyboard.ViewModels
 {
@@ -20,15 +23,15 @@ namespace Keyboard.ViewModels
             InitializeCommands();
         }
 
+        #region Private methods
         private void InitializeCommands()
         {
             AddCharCommand = new DelegateCommand<string>(_AddChar);
-
             DeleteCharCommand = new DelegateCommand(_DeleteChar);
-
             ClearCommand = new DelegateCommand(_Clear);
-
             AddNumberCommand = new DelegateCommand(_AddNumber);
+            JsonSerialize = new DelegateCommand(_JsonSerializeNow);
+            JsonDeserialize = new DelegateCommand(_JsonDeserializeNow);
         }
 
         private void _AddChar(string key)
@@ -65,11 +68,37 @@ namespace Keyboard.ViewModels
                 _Clear();
             }
         }
+        public async void _JsonSerializeNow()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Keyboard\\NumberList\\";
+            string filePath = path + "NumberList.json";
 
+            if (!Directory.Exists(path))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path);
+            }
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+
+            using FileStream createStream = File.Create(filePath);
+            await JsonSerializer.SerializeAsync(createStream, NumberList, options);
+        }
+
+        public async void _JsonDeserializeNow()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Keyboard\\NumberList\\";
+            string filePath = path + "NumberList.json";
+            using FileStream openStream = File.OpenRead(filePath);
+            NumberList = await JsonSerializer.DeserializeAsync<ObservableCollection<NumberModel>>(openStream);
+        }
+        #endregion
+
+        #region private fields
         private string inputString = "";
         private string displayText = "";
         private char[] specialChars = { '*', '#' };
         private ObservableCollection<NumberModel> numberList = new();
+        #endregion
 
         #region Public properties
         public string InputString
@@ -83,14 +112,17 @@ namespace Keyboard.ViewModels
             get => displayText;
             set => SetProperty(ref displayText, value);
         }
-        public ObservableCollection<NumberModel> NumberList { get => numberList; set => numberList = value; }
+        public ObservableCollection<NumberModel> NumberList { get => numberList; set => SetProperty(ref numberList, value); }
 
         public ICommand AddCharCommand { get; set; }
         public ICommand DeleteCharCommand { get; set; }
         public ICommand ClearCommand { get; set; }
         public ICommand AddNumberCommand { get; set; }
+        public ICommand JsonSerialize { get; set; }
+        public ICommand JsonDeserialize { get; set; }
         #endregion
 
+       /* 
         private string FormatText(string str)
         {
             bool hasNonNumbers = str.IndexOfAny(specialChars) != -1;
@@ -111,5 +143,6 @@ namespace Keyboard.ViewModels
             }
             return formatted;
         }
+       */
     }
 }
